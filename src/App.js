6875950b18dde5eb51add28c7250c9b2f205f14c
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-08-11 12:45:15 -0300

    feat: add user context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import UserProvider from "./providers/user";
 import Routes from "./routes";
 import Global from "./styles/global";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <Global />
       <Routes />
-    </>
+    </UserProvider>
   );
 }
 
---
+++ src/providers/user/index.js
@@ -0,0 +1,58 @@
+import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../../components/services";
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  function login(loginData) {
+    api.post("sessions", loginData).then(({ data: { user, token } }) => {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      localStorage.removeItem("@kenzie-hub:token");
+      localStorage.setItem("@kenzie-hub:token", JSON.stringify(token));
+
+      setUser(user);
+      navigate("/home", { replace: true });
+    });
+  }
+
+  function signIn(signInData) {
+    api.post("users", signInData).then(() => {
+      login(signInData);
+    });
+  }
+
+  function getProfile() {
+    api.get("profile").then(({ data }) => setUser(data));
+  }
+
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = JSON.stringify(localStorage.getItem("@kenzie-hub:token"));
+
+    if (token) {
+      try {
+        api.defaults.headers.authorization = `Bearer ${token}`;
+
+        getProfile();
+      } catch ({ response }) {
+        console.error(response);
+      }
+    }
+
+    setLoading(false);
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, loading, login, signIn, getProfile }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
