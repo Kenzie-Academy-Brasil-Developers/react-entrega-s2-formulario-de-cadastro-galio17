b25ccf8e14e11411a26d5e6df40edf79f61ecc17
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-08-15 09:08:04 -0300

    add click event in modal component

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { ThemeContainer } from "../../styles/containers";
 
 const Modal = ({ children, title }) => {
+  const modalContent = useRef();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    function handleOutClick(event) {
+      if (!modalContent.current.contains(event.target)) {
+        navigate("/", { replace: true });
+      }
+    }
+
+    document.addEventListener("mousedown", handleOutClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutClick);
+    };
+  }, [navigate]);
+
   return (
     <div>
-      <ThemeContainer>
+      <ThemeContainer ref={modalContent}>
         <div>
           <h4>{title}</h4>
-          <button>excluir</button>
+          <Link to="/" replace>
+            <button>excluir</button>
+          </Link>
         </div>
         {children}
       </ThemeContainer>
